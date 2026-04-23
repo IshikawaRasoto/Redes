d85a2b764d6ad60d01e2730a7c42a841a37f3900
IshikawaRasoto <ishikawarasoto@gmail.com>
2026-04-23 15:43:30 -0300

    Testes em preparacao para envio do arquivo

--- tarefa1/client/client.py
+++ tarefa1/client/client.py
@@ -1,19 +1,40 @@
+import argparse
 import socket
+import os
+import struct
+import hashlib
 
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 8080
 
-# AF_INET = IPv4 | SOCK_DGRAM = Protocolo UDP 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def main():
+    # 1. Configurar o analisador de argumentos
+    parser = argparse.ArgumentParser(description="Client para transferência de arquivos usando UDP")
+    
+    # 2. Adicionar o parâmetro/flag --params
+    parser.add_argument('--serverip', type=str, help='IP do servidor')
 
-mensagem = "Hello do Cliente!"
-print(f"[*] Enviando para {SERVER_HOST}:{SERVER_PORT} -> '{mensagem}'")
+    # 3. Adicionar o parâmetro/flag --serverport
+    parser.add_argument('--serverport', type=int, help='Porta do servidor')
 
-client_socket.sendto(mensagem.encode('utf-8'), (SERVER_HOST, SERVER_PORT))
+    parser.add_argument('--filename', type=str, help='Nome do arquivo para baixar')
 
-# Aguarda a resposta do servidor
-data, server_address = client_socket.recvfrom(1024)
-print(f"[+] Resposta do servidor {server_address}: {data.decode('utf-8')}")
+    # 3. Analisar os argumentos da linha de comando
+    args = parser.parse_args()
+    
+    # AF_INET = IPv4 | SOCK_DGRAM = Protocolo UDP 
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Fecha o socket no cliente
-client_socket.close()
+    mensagem = "GET /" + args.filename
+
+    print(f"[*] Enviando para {args.serverip}:{args.serverport} -> '{mensagem}'")
+
+    client_socket.sendto(mensagem.encode('utf-8'), (args.serverip, args.serverport))
+
+    # Aguarda a resposta do servidor
+    data, server_address = client_socket.recvfrom(1024)
+    print(f"[+] Resposta do servidor {server_address}: {data.decode('utf-8')}")
+
+    # Fecha o socket no cliente
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
--- tarefa1/server/server.py
+++ tarefa1/server/server.py
@@ -1,24 +1,105 @@
+import argparse
 import socket
+import os
+import struct
+import hashlib
+import threading
 
-HOST = '127.0.0.1' 
-# Porta maior que 1024, conforme exigido no trabalho 
-PORT = 8080        
-
-# AF_INET = IPv4 | SOCK_DGRAM = Protocolo UDP 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Vincula o socket ao endereço e porta escolhidos
-server_socket.bind((HOST, PORT))
-
-print(f"[*] Servidor UDP escutando em {HOST}:{PORT}...")
-
-while True:
-    data, client_address = server_socket.recvfrom(1024)
+def main():
+    HOST = '127.0.0.1' 
+    # Porta maior que 1024, conforme exigido no trabalho 
     
-    # Decodifica os bytes recebidos para string
-    mensagem = data.decode('utf-8')
-    print(f"[+] Mensagem recebida de {client_address}: {mensagem}")
+    argparser = argparse.ArgumentParser(description="Servidor para transferência de arquivos usando UDP")
+    argparser.add_argument('--port', type=int, help='Porta do servidor')
+    args = argparser.parse_args()
+    PORT = args.port
 
-    # Envia uma resposta de volta para o endereço do cliente
-    resposta = "ACK: Hello recebido pelo Servidor!"
-    server_socket.sendto(resposta.encode('utf-8'), client_address)
+
+    # AF_INET = IPv4 | SOCK_DGRAM = Protocolo UDP 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Vincula o socket ao endereço e porta escolhidos
+    server_socket.bind((HOST, PORT))
+
+    print(f"[*] Servidor UDP escutando em {HOST}:{PORT}...")
+
+    while True:
+        data, client_address = server_socket.recvfrom(1024)
+        
+        # Decodifica os bytes recebidos para string
+        mensagem = data.decode('utf-8')
+        print(f"[+] Mensagem recebida de {client_address}: {mensagem}")
+
+        # Tratamento da requisição:
+        partes = mensagem.strip().split()
+
+        if len(partes) != 2 or partes[0] != "GET":
+            resposta = "400 Bad Request: formato esperado 'GET /{filename}.ext'"
+        else:
+            caminho_requisitado = partes[1]
+
+            if not caminho_requisitado.startswith("/"):
+                resposta = "400 Bad Request: caminho invalido"
+            else:
+                nome_arquivo = caminho_requisitado[1:]
+
+                if not nome_arquivo or "/" in nome_arquivo or "\\" in nome_arquivo:
+                    resposta = "400 Bad Request: nome de arquivo invalido"
+                else:
+                    caminho_arquivo = os.path.join("files", nome_arquivo)
+
+                    if os.path.isfile(caminho_arquivo):
+                        print(f"Arquivo identificado, criando thread para cliente {client_address}...")
+                        thread = threading.Thread(target=handle_client, args=(client_address[0], client_address[1], caminho_arquivo))
+                        thread.start()
+                    else:
+                        resposta = f"404 Not Found: arquivo '{nome_arquivo}' nao existe"
+
+
+def handle_client(client_ip: str, client_port: int, filename: str):
+    thread_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    seq_num = 0
+
+    thread_socket.bind(('', 0))  # Bind a uma porta aleatória disponível
+    meu_ip, minha_porta = thread_socket.getsockname()
+    print(f"[Thread] Iniciada para {client_ip}:{client_port} usando porta {minha_porta}")
+
+    thread_socket.sendto(gerar_mensagem_comeco_transmissao(seq_num), (client_ip, client_port))
+    seq_num += 1
+    
+    transfering = True
+    while transfering:
+        # Lógica de transferência de arquivos usando mensagens estruturadas
+        pass
+
+
+# Tipos de mensagem:
+# 0 - Começando transmissão : 'B'
+# 1 -> n-1 - Segmentos de dados : 'D'
+# n - End of File : 'E'
+# 
+# Acknowledgement: 'A' 
+
+# Estrutura da mensagem
+# Cabecalho: 1 byte (tipo) + 4 bytes (numero de sequencia ou tamanho do nome do arquivo) + 16 bytes MD5 hash + data (variavel, dependendo do tipo)
+
+def gerar_mensagem_ack(seq_num: int):
+    return struct.pack('!cI', b'A', seq_num)
+
+def gerar_mensagem_comeco_transmissao(seq_num: int):
+    mensagem = struct.pack('!cI', b'B', seq_num)
+    return mensagem
+
+
+def gerar_mensagem_dados(seq_num, dados):
+    return struct.pack('!cI', b'D', seq_num) + dados
+
+def gerar_mensagem_eof(seq_num):
+    return struct.pack('!cI', b'E', seq_num)
+
+def calcular_checksum(dados):
+    return hashlib.md5(dados).hexdigest()
+
+if __name__ == "__main__":
+    main()
